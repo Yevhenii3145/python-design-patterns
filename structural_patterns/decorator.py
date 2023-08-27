fa1f3bcc35db9852c213427ac25828e9cc08f807
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-08-27 21:46:03 +0300

    structural_pattern __decorator

---
+++ structural_patterns/decorator.py
@@ -0,0 +1,53 @@
+"""Декоратор — это структурный паттерн, который позволяет добавлять объектам
+новые поведения на лету, помещая их в объекты-обёртки."""
+
+
+class Component():
+    def operation(self) -> str:
+        pass
+
+
+class ConcreteComponent(Component):
+    def operation(self) -> str:
+        return "ConcreteComponent"
+
+
+class Decorator(Component):
+    _component: Component = None
+
+    def __init__(self, component: Component) -> None:
+        self._component = component
+
+    @property
+    def component(self) -> Component:
+        return self._component
+
+    def operation(self) -> str:
+        return self._component.operation()
+
+
+class ConcreteDecoratorA(Decorator):
+    def operation(self) -> str:
+        return f"ConcreteDecoratorA({self.component.operation()})"
+
+
+class ConcreteDecoratorB(Decorator):
+    def operation(self) -> str:
+        return f"ConcreteDecoratorB({self.component.operation()})"
+
+
+def client_code(component: Component) -> None:
+    print(f"RESULT: {component.operation()}", end="")
+
+
+if __name__ == "__main__":
+    simple = ConcreteComponent()
+    print("Client: I've got a simple component:")
+    client_code(simple)  # RESULT: ConcreteComponent
+    print("\n")
+
+    decorator1 = ConcreteDecoratorA(simple)
+    decorator2 = ConcreteDecoratorB(decorator1)
+    print("Client: Now I've got a decorator component:")
+    # RESULT: ConcreteDecoratorB(ConcreteDecoratorA(ConcreteComponent))
+    client_code(decorator2)
