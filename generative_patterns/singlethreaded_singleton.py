8b0551671e3f94b8ac982aa97b19be0f0e9c3822
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-08-29 00:11:00 +0300

    generative_patterns__singleton

---
+++ generative_patterns/singlethreaded_singleton.py
@@ -0,0 +1,27 @@
+"""Одиночка — это порождающий паттерн, который гарантирует существование только одного объекта определённого класса, а также позволяет достучаться до этого объекта из любого места программы."""
+
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Singleton(metaclass=SingletonMeta):
+    def some_business_logic(self):
+        pass
+
+
+if __name__ == "__main__":
+    s1 = Singleton()
+    s2 = Singleton()
+
+    # Singleton works, both variables contain the same instance.
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances")
